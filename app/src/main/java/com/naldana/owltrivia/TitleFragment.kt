1a248888419df354109e78ecee4956341132595a
00400815@uca.edu.sv <16137238>
2020-05-08 17:16:49 -0600

    navhost...

---
+++ app/src/main/java/com/naldana/owltrivia/TitleFragment.kt
@@ -0,0 +1,26 @@
+package com.naldana.owltrivia
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.naldana.owltrivia.databinding.FragmentTitleBinding
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class TitleFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        //return inflater.inflate(R.layout.fragment_title, container, false)
+        val databinding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater, R.layout.fragment_title,container,false)
+        return databinding.root
+    }
+
+}
