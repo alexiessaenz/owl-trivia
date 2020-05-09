70d38c801f5774a7c7e3436bcf28071926b65078
00400815@uca.edu.sv <16137238>
2020-05-08 22:52:52 -0600

    gameFragment config finsh....

--- app/src/main/java/com/naldana/owltrivia/GameFragment.kt
+++ app/src/main/java/com/naldana/owltrivia/GameFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.naldana.owltrivia.databinding.FragmentGameBinding
 
@@ -17,7 +18,7 @@ class GameFragment : Fragment() {
         val text:String,
         val answers:List<String>
     )
-    private val question: MutableList<Question> = mutableListOf(
+    private val questions: MutableList<Question> = mutableListOf(
         Question(
             text = "pregunta 1",
             answers = listOf("Buena","mala 1", "mala 2", "mala 3" )
@@ -54,21 +55,69 @@ class GameFragment : Fragment() {
     )
 
     lateinit var currentQuestion: Question
-    lateinit var answer: MutableList<String>
+    lateinit var answers: MutableList<String>
     private var questionIndex = 0
-    private val numQuestion = Math.min((question.size + 1)/2, 3)
+    private val numQuestions = Math.min((questions.size + 1)/2, 3)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val binding = DataBindingUtil.inflate<FragmentGameBinding>(inflater,R.layout.fragment_game,container,false )
+        val binding = DataBindingUtil.inflate<FragmentGameBinding>(
+            inflater,
+            R.layout.fragment_game,
+            container,
+            false
+        )
 
         randomizeQuestions()
 
-        binding.game=this
-        return inflater.inflate(R.layout.fragment_game, container, false)
+        binding.game = this
+
+        binding.submitButton.setOnClickListener {
+            val checkId = binding.radioGroupQuestion.checkedRadioButtonId
+
+            if (-1 != checkId) {
+                var answerIndex = 0
+                when (checkId) {
+                    R.id.radioOptionB -> answerIndex = 1
+                    R.id.radioOptionC -> answerIndex = 2
+                    R.id.radioOptionD -> answerIndex = 3
+                }
+                if (answers[answerIndex] == currentQuestion.answers[0]) {
+                    answerIndex++
+
+                    if (questionIndex < numQuestions) {
+                        currentQuestion = questions[questionIndex]
+                        setQuestion()
+                        binding.invalidateAll()
+                    } else {
+                        //win
+                    }
+                } else {
+                    //loose
+                }
+
+            }
+
+
+        }
+
+        return binding.root//inflater.inflate(R.layout.fragment_game, container, false)
     }
 
+    private fun randomizeQuestions(){
+        questions.shuffle()
+        questionIndex = 0
+        setQuestion()
+    }
+
+    private fun setQuestion(){
+        currentQuestion = questions[questionIndex]
+        answers = currentQuestion.answers.toMutableList()
+        answers.shuffle()
+        (activity as AppCompatActivity).supportActionBar?.title = getString(R.string.title_owl_trivia_question,
+            questionIndex+1,numQuestions)
+    }
 }
